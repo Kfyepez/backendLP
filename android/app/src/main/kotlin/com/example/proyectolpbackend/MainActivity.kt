58bd6dee78f579e2505ae997f1f04876f8bafbf0
santiago1617 <saantumb@espol.edu.ec>
2022-01-24 21:24:52 -0500

    server fixed

--- android/app/src/main/kotlin/com/example/proyectolpbackend/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proyectolpbackend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- routes/index.js
+++ routes/index.js
@@ -13,54 +13,64 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/personas', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   Persona.findAll({
       attributes: { exclude: ["updatedAt","createdAt"] }
   })
   .then(personas => {
-      res.send(personas);
+      res.send({"perosnas": personas});
   })
   .catch(error => res.status(400).send(error))
 
 }); 
 
 router.get('/recomendaciones', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   Recomendacion.findAll({
       attributes: { exclude: ["updatedAt","createdAt"] }
   })
   .then(recomendaciones => {
-      res.send(recomendaciones);
+      res.send({'recomendaciones':recomendaciones});
   })
   .catch(error => res.status(400).send(error))
 
 }); 
 
 router.get('/destinos_turisticos', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   Destino.findAll({
       attributes: { exclude: ["updatedAt","createdAt"] }
   })
   .then(destinos => {
-      res.send(destinos);
+      res.send({"destinos":destinos});
   })
   .catch(error => res.status(400).send(error))
 
 }); 
 router.get('/imagenes', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   Imagen.findAll({
       attributes: { exclude: ["updatedAt","createdAt"] }
   })
   .then(imagenes => {
-      res.send(imagenes);
+      res.send({"imagenes" : imagenes});
   })
   .catch(error => res.status(400).send(error))
 
 }); 
 
 router.get('/rutas', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   Ruta.findAll({
       attributes: { exclude: ["updatedAt","createdAt"] }
   })
   .then(rutas => {
-      res.send(rutas);
+      res.send({"rutas":rutas});
   })
   .catch(error => res.status(400).send(error))
 
